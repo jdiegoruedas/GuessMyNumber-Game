054aa47fbfbd8c357303d246731978a48e10c6d1
jdiegoruedas <jdiegoruedas@gmail.com>
2021-09-28 17:18:28 -0300

    Final commit. Still no make it more nicer, but it works

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
+const elements = document.querySelectorAll('.sign-animation');
+const rotateElements = function () {
+  elements.forEach(elements => {
+    elements.classList.add('rotate');
+  });
+  console.log(elements);
+};
+
 document.querySelector('.start').addEventListener('click', function () {
   const begin = document.querySelector('.game-begin');
   begin.classList.add('hide');
+  rotateElements();
 });
-
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
-
-/* 
-const livesCountdown = function () {
-  let lives = 15;
-  document.querySelector('.lives').textContent = `Lives: ${lives--}`;
-}; */
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
+};
+
+const livesCountdown = function () {
+  lives--;
+  document.querySelector('.lives').textContent = `Lives: ${lives}`;
+};
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let lives = 15;
+
+const guessRange = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  '?',
+];
+
+const changeNumbers = function () {
+  let timer = 0;
+
+  for (let i = 0; i < guessRange.length; i++) {
+    setTimeout(() => {
+      document.querySelector('.number').innerHTML = guessRange[i];
+    }, timer);
+
+    timer = timer + 20;
+  }
 };
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -21,13 +66,30 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     displayMessage('No number!');
   } else if (guess !== secretNumber) {
+    changeNumbers();
     if (guess > secretNumber) {
       displayMessage('Too high!');
+      livesCountdown();
     }
     if (guess < secretNumber) {
       displayMessage('Too low!');
+      livesCountdown();
     }
   } else {
+    const winNumberChange = document.querySelector('.number');
+    winNumberChange.textContent = secretNumber;
+    winNumberChange.style.width = '30rem';
     displayMessage('You win!');
+    document.querySelector('.check').textContent = 'Congratz!';
+  }
+
+  if (lives === 0) {
+    displayMessage('You lost!');
+    const check = document.querySelector('.check');
+    check.textContent = 'Try again!';
+
+    document.querySelector('.check').addEventListener('click', function () {
+      location.reload();
+    });
   }
 });
